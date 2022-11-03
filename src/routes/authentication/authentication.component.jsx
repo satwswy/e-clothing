6c88bb138d68926e124a88ec06ccef98bc02b8bd
marios <mgeorgak859@gmail.com>
2022-11-03 21:41:03 +0200

    renames sign-in

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,7 +1,7 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
-const SignIn = () => {
+const Authentication = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -19,4 +19,4 @@ const SignIn = () => {
     )
 }
 
-export default SignIn
+export default Authentication
