432d84926b3b168f91f78dbf404b91a35ef149e3
marios <mgeorgak859@gmail.com>
2022-11-04 16:18:35 +0200

    adds util sign out updates component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,17 @@
-import { Fragment , useContext} from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import './navigation.styles.scss'
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 const Navigation = () => {
 
 
-    const {currentUser} = useContext(UserContext)
-
+    const { currentUser, setCurrentUser } = useContext(UserContext)
+    const signOutHandler = async () => {
+         await signOutUser();
+         setCurrentUser(null);
+    }
     return (
         <Fragment>
             <div className="navigation">
@@ -18,9 +22,14 @@ const Navigation = () => {
                     <Link className="nav-link" to='/shop'>
                         SHOP
                     </Link>
-                    <Link className="nav-link" to='/auth'>
-                        SIGN IN
-                    </Link>
+                    {
+                        currentUser ? (
+                            <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>)
+                            : (<Link className="nav-link" to='/auth'>
+                                SIGN IN
+                            </Link>)
+                    }
+
                 </div>
             </div>
             <Outlet />
