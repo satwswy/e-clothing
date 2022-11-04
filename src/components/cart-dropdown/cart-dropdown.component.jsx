68bb0148e1a6ff4248834f7dbc3a23fb3d6dbaab
marios <mgeorgak859@gmail.com>
2022-11-04 22:15:55 +0200

    Update cart-dropdown.component.jsx

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,7 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = () => (
   <div className='cart-dropdown-container'>
     <div className='cart-items' />
-    <Button>GO TO CHECKOUT</Button>
+    <Button>CHECKOUT</Button>
   </div>
 );
 
