35962e8f6882bfd858581c1f72db3c666da1d3c8
marios <mgeorgak859@gmail.com>
2022-11-04 14:15:54 +0200

    updates auth

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,11 +1,11 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import './authentication.styles.scss'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 const Authentication = () => {
 
 
     return (
-        <div>
+        <div className='authentication-container'>
             <h1>Sign In</h1>
             <SignInForm />
             <SignUpForm />
