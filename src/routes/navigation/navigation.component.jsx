a45441d5f595043e7c0e471ccc99ca86e7d9f1d0
marios <mgeorgak859@gmail.com>
2022-11-04 22:10:17 +0200

    updates

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,21 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import './navigation.styles.scss'
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import { UserContext } from "../../contexts/user.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-
+import './navigation.styles.scss'
 
 
 const Navigation = () => {
 
 
     const { currentUser } = useContext(UserContext)
-
+    const {isCartOpen} = useContext(CartContext)
     return (
         <Fragment>
             <div className="navigation">
@@ -34,7 +35,7 @@ const Navigation = () => {
                     }
                     <CartIcon />
                 </div>
-                <CartDropdown />
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
