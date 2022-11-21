f54e7facb8bbb4863cdbf59c629be69fbc306c0a
marios <mgeorgak859@gmail.com>
2022-11-21 18:52:32 +0200

    updates categories context to redux store

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,22 @@
-import { useContext, Fragment } from 'react'
-import CategoryPreview from '../../components/category-preview/category-preview.component'
-import { CategoriesContext } from '../../contexts/categories.context'
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { selectCategoriesMap } from '../../store/categories/categories.selector';
 
 const CategoriesPreview = () => {
+  const categoriesMap = useSelector(selectCategoriesMap);
 
-    const { categoriesMap } = useContext(CategoriesContext)
-    return (
-        <Fragment>
-            {
-                Object.keys(categoriesMap).map(title => {
-                    const products = categoriesMap[title];
-                    return <CategoryPreview key={title} title={title} products={products}/>
-                })}
-        </Fragment>
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </Fragment>
+  );
+};
 
-    )
-}
-
-export default CategoriesPreview
+export default CategoriesPreview;
