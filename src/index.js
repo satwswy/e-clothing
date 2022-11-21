c29ed51292c24c05492b8d8f0ad027744487ace1
marios <mgeorgak859@gmail.com>
2022-11-21 19:54:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
 
 import './index.scss';
 
-const rootElement = document.getElementById('root');
 
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -21,5 +23,9 @@ render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  rootElement
+
 );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
