a229cd7d13ad55d893bfac272ef072bca2a462fa
marios <mgeorgak859@gmail.com>
2022-11-22 21:24:35 +0200

    adds redux-persist updates store

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import App from './App';
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
-import './index.scss';
-
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-
+  </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
